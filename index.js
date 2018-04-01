05c2eba256baff8bfa7d3329248a4cab7e22cbaa
Chaudhry Junaid Anwar <junaidanwar10@gmail.com>
2018-04-01 16:20:09 +0500

    add compatible retry function

--- index.js
+++ index.js
@@ -14,9 +14,22 @@ console.log('Connecting redis...');
 if (args.retry_strategy) {
 	console.log('Using retry_strategy...');
 	redisOptions = _.omit(redisOptions, ['retry_max_delay', 'connect_timeout', 'max_attempts'])
-	redisOptions.retry_strategy(function(options) {
-		return 0;
-	});
+	redisOptions.retry_strategy = function(options) {
+		if (args.connect_timeout && options.total_retry_time > args.connect_timeout) {
+			// End reconnecting after a specific timeout and flush all commands
+			// with a individual error
+			return new Error('Retry time exhausted');
+		}
+		if (args.max_attempts && options.attempt >= args.max_attempts) {
+			// End reconnecting with built in error
+			return new Error('Max retry attempts exhausted');
+		}
+		if (args.retry_max_delay) {
+			return Math.min(Math.pow(2, options.attempt+7), args.retry_max_delay);
+		}
+		// reconnect after
+		return Math.pow(2, options.attempt+7);
+	};
 	redis.createClient(redisOptions)
 } else {
 	console.log('Using deprecated retry params...');
